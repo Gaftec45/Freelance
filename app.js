0ac1312b0f436e919a7e86b2bd7d8991a3ae632e
GFT <gaaftechsolution@gmail.com>
2024-03-03 18:27:31 +0100

    initial commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/MY-REACTAPP')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
