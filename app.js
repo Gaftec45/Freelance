d8a8c4c54c05f7f3ee5b631138b5209976e234bc
GFT <gaaftechsolution@gmail.com>
2024-03-03 20:31:32 +0100

    initial commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-  origin: RED ||'http://localhost:3000'
+  origin: RED ||'https://tega-d.onrender.com'
 })); // Apply CORS middleware
 
 // Parse JSON bodies
